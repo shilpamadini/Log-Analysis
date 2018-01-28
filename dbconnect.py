ac17c8a6ca5824c481c79f8351186291ed3c04ea
Shilpa Madini <shilpa.madini@gmail.com>
2018-01-27 21:51:44 -0800

    Python file for class Connect

---
+++ dbconnect.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Python code to create Dbconnect class."""
+
+import psycopg2
+import sys
+
+
+class Connect():
+    """
+    This class provides blueprint to connect to database
+    connection and execute statements.
+    """
+
+    def __init__(self, db_name, sql):
+        """
+        Constructor for the Dbconnet instance
+        :param db_name: string
+        :param sql: string
+        """
+        self.dbname = db_name
+        self.sql_statement = sql
+
+    def connect_db(self):
+        """
+        Function connects to database, executes the sql statement
+        and returns results.
+        """
+        try:
+            db = psycopg2.connect(database=self.dbname)
+            cursor = db.cursor()
+            cursor.execute(self.sql_statement)
+            rows = cursor.fetchall()
+            db.close()
+            return rows
+        except psycopg2.OperationalError as e:
+            print(str(e)+"\n")
+            sys.exit(1)
+        except psycopg2.Error as e:
+            print("verify your sql statement, "+e.diag.message_primary+"\n")
+            sys.exit(1)
