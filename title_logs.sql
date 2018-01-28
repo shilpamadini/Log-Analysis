43b455a6975c54a4073e4e9e9466ac7be0db3815
Shilpa Madini <shilpa.madini@gmail.com>
2018-01-27 21:54:19 -0800

    changed the join condition in the where clause

--- title_logs.sql
+++ title_logs.sql
@@ -2,4 +2,4 @@
 
 create view title_logs as
 (select ar.author,ar.title,ar.slug,lg.path,lg.ip,lg.method,lg.status,lg.time,lg.id from articles ar , log lg
-where lg.path like '%'||ar.slug||'%' order by ar.title);
+where lg.path = concat('/article/',ar.slug) order by ar.title);
